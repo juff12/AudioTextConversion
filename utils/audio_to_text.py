a8ffa54ca76f4e3c55f4c23697ed23a8fac6f9fe
jmuff12 <jonathan@dtacgear.net>
2024-05-06 10:46:18 -0400

    readded file to support only audio to text

---
+++ utils/audio_to_text.py
@@ -0,0 +1,72 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import os
+import json
+from tqdm import tqdm
+import argparse
+
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, default='data/streamers/test', help='directory of files to be processed')
+    parser.add_argument('--reprocess_old', type=bool, default=True, help='reprocess old files')
+    return parser.parse_args()
+
+def get_audio_files(directory, audio_file_endings):
+    audio_files = []
+    for file in os.listdir(directory):
+        for ending in audio_file_endings:
+            if file.endswith(ending):
+                audio_files.append(os.path.join(directory, file))
+    if len(audio_files) == 0:
+        return None
+    return audio_files[0] # return the first audio file
+
+def main():
+    opt = args()
+    
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model_id = "openai/whisper-large-v3"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    directory = opt.dir
+
+    sub_dirs = os.listdir(directory)
+
+    # audio file endings
+    audio_file_endings = ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a']
+    
+    for dir in tqdm(sub_dirs):
+        # skip already processed files
+        if opt.reprocess_old is False and os.path.exists(os.path.join(directory,f"{dir}/audio_text_{dir}.json")):
+            continue
+        try:
+            audio_file = get_audio_files(os.path.join(directory, dir), audio_file_endings)
+            result = pipe(audio_file, generate_kwargs={"language": "english"})
+            with open(os.path.join(directory,f"{dir}/audio_text_{dir}.json"), "w") as f:
+                json.dump(result, f, indent=4)
+        except:
+            print('Error')
+            continue # if an error occurs while processing, continue processing
+if __name__=='__main__':
+    main()
