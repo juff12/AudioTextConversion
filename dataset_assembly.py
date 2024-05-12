384dddc76df7bddc617b4593f20e6b5a2b48e978
jmuff12 <jonathan@dtacgear.net>
2024-05-12 19:54:59 -0400

    added argparse arguments, fixed bugs in data_type filename creation

--- dataset_assembly.py
+++ dataset_assembly.py
@@ -2,13 +2,13 @@ import json
 import os
 import argparse
 from pathlib import Path
-from utils.functions import prep_data
 import re
 
 def args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default='data/streamers/test', help='directory of files to be processed')
-    parser.add_argument('--output', type=str, default='data/datasets/test/final_data.json', help='output location for the dataset')
+    parser.add_argument('--output_dir', type=str, default='data/datasets/test/', help='output location for the dataset')
+    parser.add_argument('--outfile', type=str, default='final_data.json', help='output file name')
     parser.add_argument('--lower', type=bool, default=False, help='lowercase the text')
     parser.add_argument('--file_type', type=str, default='txt', help='the type of file the preprocessed data is in')
     parser.add_argument('--data_type', type=str, default='clusters', help='type of dataset to be processed [default][clusters/chat_response/pairs]')
@@ -17,8 +17,8 @@ def args():
     # eos, bos, inst tokens
     parser.add_argument('--eos', type=str, default='</s>', help='end of sentence token')
     parser.add_argument('--bos', type=str, default='<s>', help='beginning of sentence token')
-    parser.add_argument('--inst', type=str, default='', help='instruction token')
-
+    parser.add_argument('--inst_start', type=str, default='', help='instruction token start')
+    parser.add_argument('--inst_end', type=str, default='', help='instruction token end')
     
     return parser.parse_args()
 
@@ -31,9 +31,16 @@ def main():
     # file extension
     ext = '.' + opt.file_type
 
+    filename = '-------------' # placeholder for the filename
     # get all the files in the directory
     subs = [os.path.join(opt.dir, sub) for sub in os.listdir(opt.dir)]
-    files = [os.path.join(sub, f) for sub in subs for f in os.listdir(sub) if f'{opt.data_type}_{opt.time_cutoff}' in f and f.endswith(ext)]
+    if opt.data_type == 'clusters':
+        filename = f'{opt.data_type}_{opt.time_cutoff}'
+    elif 'chat_response' in opt.data_type or 'pairs' in opt.data_type:
+        filename = f'{opt.data_type}'
+
+
+    files = [os.path.join(sub, f) for sub in subs for f in os.listdir(sub) if filename in f and f.endswith(ext)]
     
     final_data = []
     for f in files:
@@ -42,10 +49,10 @@ def main():
         # load the data and prep for clusters
         if opt.data_type == 'clusters':
             with open(f, 'r') as file:
-                if opt.type_data == 'json':
+                if opt.file_type == 'json':
                     data = json.load(file)
                     data = [item['text'] for item in data if item['text'] != '']
-                elif opt.type_data == 'txt': # open the txt file
+                elif opt.file_type == 'txt': # open the txt file
                     data = file.read()
                     data = data.split('\n')
                     # create json style object
@@ -57,24 +64,32 @@ def main():
                 text = re.sub(r'\s+', ' ', text)
                 data[i] = text
         # load and prep the data for chat response
-        elif opt.data_type == 'chat_response' or opt.data_type == 'pairs':
+        elif 'chat_response' in opt.data_type or 'pairs' in opt.data_type:
             with open(f, 'r') as file:
                 data = json.load(file)
             # add the BOS and EOS tokens
             for i, item in enumerate(data):
+                response = item['response'].strip()
+                message = item['message'].strip()
+                
+                
                 # missing response or message, add only the available one
-                if item['response'] == '' or item['message'] == '':
-                    text = f'{opt.bos} {item["message"].strip()} {item['response'].strip()} {opt.eos}'
+                if response == '' or message == '':
+                    text = f'{opt.bos} {message} {response} {opt.eos}'
                 else:
-                    text = f'{opt.bos} {item["message"].strip()} {opt.inst} {item['response'].strip()} {opt.eos}'
+                    text = f'{opt.bos}{opt.inst_start} {message} {opt.inst_end} {response} {opt.eos}'
                 # remove multiple spaces
                 text = re.sub(r'\s+', ' ', text)
                 data[i] = text
         # add the text to the final data
         final_data += data
 
+    # format the final data
+    final_data = [{'text': item} for item in final_data if item != '']
+
+
     # save the final data
-    with open(opt.output, 'w') as file:
+    with open(os.path.join(opt.output_dir, opt.outfile), 'w') as file:
         json.dump(final_data, file, indent=4)
 
 if __name__=="__main__":
